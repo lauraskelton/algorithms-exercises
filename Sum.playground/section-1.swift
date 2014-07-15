eaa741ecd4edefc47f63268e6913bfb3d3962e77
Laura Skelton <lauraeskelton@gmail.com>
2014-07-15 12:24:57 -0400

    Updated Recursion

--- Sum.playground/section-1.swift
+++ Sum.playground/section-1.swift
@@ -1,15 +1,28 @@
 // Recursion Exercises
 // Sum of items in a list
 
-func listSum (numbers:[Int], sum_so_far:Int) -> Int {
+// tail call
+func listSumTail (numbers:[Int], sum_so_far:Int) -> Int {
     if numbers.count == 0 {
         return sum_so_far
     }
     var newSum = sum_so_far + numbers[0]
     var nextNumbers = numbers
     nextNumbers.removeAtIndex(0)
-    return listSum(nextNumbers, newSum)
+    return listSumTail(nextNumbers, newSum)
 }
 
-listSum([1,2,3,4,5], 0)
-listSum([], 0)
+func listSum (numbers: [Int]) -> Int {
+    if numbers.count == 0 {
+        return 0
+    }
+    var nextNumbers = numbers
+    var thisNum = nextNumbers.removeAtIndex(0)
+    return thisNum + listSum(nextNumbers)
+}
+
+listSumTail([1,2,3,4,5], 0)
+listSumTail([], 0)
+
+listSum([1,2,3,4,5])
+
